1354a8ad088aa4903023b363d5708febc52363c1
liq <64891712+wosli@users.noreply.github.com>
2025-02-20 23:02:41 +0800

    updata

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,15 +9,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# drop mosdns and v2ray-geodata packages that come with the source
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-#git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
-
-# 更新 golang 1.23 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang

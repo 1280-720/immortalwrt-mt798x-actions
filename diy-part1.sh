b88ef182f127de6b4d5907b6f6512d8b88ca85b8
liq <64891712+wosli@users.noreply.github.com>
2025-02-20 21:02:25 +0800

    updata 23.05

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,12 +9,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-
-
